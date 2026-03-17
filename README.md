0a688abe45347c0b8082ccb3aaa40f8a0360eae4
Voorde, Ted van der (ITOP AT) - KL <Ted-van-der.Voorde+afklm@klm.com>
2026-03-17 12:04:10 +0100

    test

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # myapp
 myapp
+
+
+rg-linc-state
+
+salinctfstate1138
+(container tfstate)
+
+rg-linc-dev
+
+rg-linc-prd
+
+rg-linc-state
+
+sub f982324c-57e0-4f62-b58d-0edf0b53dc57
+client 79fb627c-baaa-4642-8349-f4ee15ba5910
+tenant 7ca3e3aa-b312-40ea-9a83-47606334634d
+
---
+++ infra/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.49.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.7.2"
+    }
+    # azuread = {
+    #   source = "hashicorp/azuread"
+    #   #   version = "=2.47.0"
+    # }
+  }
+  backend "azurerm" {
+    container_name = "tfstate"
+    key            = "overriden-in-github-actions.yml"
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+  }
+}
+
+data "azurerm_resource_group" "infra" {
+  name = "rg-linc-${var.environment}"
+}
+
+variable "environment" {
+  default = ""
+}
+
+data "azurerm_client_config" "current" {
+}
+
+
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "asp-myapp-${var.environment}"
+  location            = data.azurerm_resource_group.infra.location
+  resource_group_name = data.azurerm_resource_group.infra.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "appservice" {
+  name                = "app-myapp-${var.environment}"
+  location            = data.azurerm_resource_group.infra.location
+  resource_group_name = data.azurerm_resource_group.infra.name
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+
+  site_config {
+    always_on = false
+    application_stack {
+      node_version = "22-lts"
+    }
+  }
+}
